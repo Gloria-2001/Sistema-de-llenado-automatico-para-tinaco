adf281756a64ea91d8a1b43af226fdbbe1c95f80
Gloria-2001 <olimen141@gmail.com>
2021-12-03 01:55:31 -0600

    Avance interfaz

---
+++ interfaz.py
@@ -0,0 +1,29 @@
+import serial#se importa la librería para conexión serial
+import time#se importa librería de tiempos
+from tkinter import * #importamos todos los métodos del paquete Tkinter para interfaz gráfica
+import tkinter.font as tkFont
+from tkinter import ttk
+
+#creacion ventana
+ventana=Tk()
+ventana.title('Proyecto')
+ventana.geometry("1200x1200")#Pone dimensiones a la ventana raíz ancho x alto
+ventana.resizable(False,False)#condiciona si se puede o no agrandar la ventana con el mouse
+ventana.iconbitmap("./img/escudoESCOM.ico")#agrega una imagen .ico a la ventana.
+
+#creacion fondo 
+background = PhotoImage(file = "./img/fondo1.png")
+canvas1 = Canvas(ventana, width = 1200,height = 1200) 
+canvas1.pack(fill = "both", expand = True)
+# Desplegar imagen
+canvas1.create_image( 0, 0, image = background, anchor = "nw")
+
+#titulo
+fontExample0 = tkFont.Font(family="Arial", size=16, weight="bold") #configuracion de la fuente
+titulo=canvas1.create_text( 600, 20, text = "Proyecto Final de la materia de Instrumentación",font=fontExample0)
+
+#subtitulo
+fontExample1 = tkFont.Font(family="Arial", size=14, weight="bold", slant="italic") #configuracion de la fuente
+subtitulo=canvas1.create_text( 600, 45, text = "Equipo 12",font=fontExample1 )
+
+ventana.mainloop()
