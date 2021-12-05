948e8be08c063f2fe02fbf814c6b3892eb5514c1
Giovanni-Hernandez <ahernandezr1612@alumno.ipn.mx>
2021-12-04 18:34:52 -0600

    segunda prueba

---
+++ Prueba/prueba2.py
@@ -0,0 +1,23 @@
+import serial#se importa la librería para conexión serial
+import time#se importa librería de tiempos
+from tkinter import *
+from tkinter.font import Font
+
+puerto=serial.Serial('COM3',9600)
+time.sleep(1)
+#puerto.open()
+
+opc = True
+while(opc):
+    enviar_PIC=input("Inserte el numero para prender o apagar el LED. 69 para encender, 65 para apagar. 66 para salir\n")
+    if(enviar_PIC != 'S'):
+        puerto.write(enviar_PIC.encode())
+        time.sleep(1)
+
+    else:
+        opc = False
+        puerto.close()
+        print("Puerto cerrado exitosamente")
+
+#print(chr(enviar_PIC).encode())
+
