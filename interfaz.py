49337e5d7015098404a0154f01b308734aed7b83
Gloria-2001 <olimen141@gmail.com>
2021-12-12 18:55:10 -0600

    Tab x2

--- interfaz.py
+++ interfaz.py
@@ -85,12 +85,6 @@ def nuevaVentana():
     Label(canvas1,text="La luminosidad es del: ", font=fontExample2, bg='white').place(x=1000,y=100)
     Label(canvas1,text="X %", font=fontExample3, borderwidth=2, relief="solid", bg='white').place(x=1050,y=125)    
 
-    #boton ir a proyecto 
-    fontPrincipal2 = tkFont.Font(family="Montserrat", size=12)
-    ir=Button(canvas0,text="Vamos al proyecto",activebackground="#A4A4A4", font=fontPrincipal2, bg="black", fg="white", command=nuevaVentana)
-    ir.config(width="20",height="2")
-    ir.place(x=490,y=350)
-
     #puerto serial
     puerto = serial.Serial('COM2',9600)
     time.sleep(1)
@@ -108,4 +102,11 @@ def nuevaVentana():
             Label(canvas1, text="       Tinaco Lleno       ", font=fontExample2, borderwidth=2, relief="solid", bg='white').place(x=80,y=490)
             Label(canvas1, text="  Faltan " + str(cm_para_llenarse) + "cm para llenarse  ", font=fontExample2, bg="#F781F3").place(x=35,y=530)
     puerto.close()
+
+#boton ir a proyecto 
+fontPrincipal2 = tkFont.Font(family="Montserrat", size=12)
+ir=Button(canvas0,text="Vamos al proyecto",activebackground="#A4A4A4", font=fontPrincipal2, bg="black", fg="white", command=nuevaVentana)
+ir.config(width="20",height="2")
+ir.place(x=490,y=350)
+
 ventana.mainloop()
